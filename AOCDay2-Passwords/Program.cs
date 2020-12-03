7bd069c2c62324e7b6f523e31b144288047ca59e
Justin Cook <justin.w.cook.civ@mail.mil>
2020-12-02 19:06:40 -0600

    fixed spelling errors

--- AOCDay2-Passwords/Program.cs
+++ AOCDay2-Passwords/Program.cs
@@ -26,16 +26,16 @@ namespace AOCDay2_Passwords
             Console.WriteLine($"Valid Password Count Using Positional Test: {validCount2}");
         }
 
-        static bool TestPasswordMinMax(int minOccurances, int maxOccurances, char character, string password)
+        static bool TestPasswordMinMax(int minOccurrences, int maxOccurrences, char character, string password)
         {
-            int occurances = password.Where(c => c == character).Count();
-            return occurances >= minOccurances && occurances <= maxOccurances;
+            int occurrences = password.Where(c => c == character).Count();
+            return occurrences >= minOccurrences && occurrences <= maxOccurrences;
         }
 
-        static bool TestPasswordPositionalOccurance(int position1, int postiion2, char character, string password)
+        static bool TestPasswordPositionalOccurance(int position1, int position2, char character, string password)
         {
             char characterAtPostion1 = password[position1 - 1];
-            char characterAtPostion2 = password[postiion2 - 1];
+            char characterAtPostion2 = password[position2 - 1];
 
             return characterAtPostion1 != characterAtPostion2 &&
                    (characterAtPostion1 == character || characterAtPostion2 == character);
